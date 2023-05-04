d603b98a66db652a1df4339c0fe44372b663af82
alansley <aclansley@gmail.com>
2023-05-04 21:22:39 +1000

    Fixes and additional code comments
    - ScriptableObject class cannot be static in Unity 2022, fixed,
    - `AssetDatabase.Refresh()` call added to avoid error on initial install, and
    - Some property renaming and a bunch of comments added to explain why things are happening (hopefully of use to developers as they learn).

--- Editor/TarodevAutoSave.cs
+++ Editor/TarodevAutoSave.cs
@@ -6,81 +6,157 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
-namespace Tarodev {
+namespace Tarodev
+{
     /// <summary>
     /// Unity has probably discussed an auto-save feature countless times over the years
     /// and decided not to implement... so take that information as you'd like. I personally
     /// like the idea and it's worked well for me during my limited testing. If you find any bugs
     /// please report them on the repo: https://github.com/Matthew-J-Spencer/Unity-AutoSave
     /// 
-    /// Love your friendly neighborhood Tarodev
+    /// Love,
+    /// Your friendly neighborhood Tarodev
     /// </summary>
     [CustomEditor(typeof(AutoSaveConfig))]
-    public class TarodevAutoSave : Editor {
-        private static AutoSaveConfig _config;
-        private static CancellationTokenSource _tokenSource;
-        private static Task _task;
+    public class TarodevAutoSave : Editor
+    {
+        /// <summary>
+        /// The configuration file that says if we should auto-save, and if so how often.
+        /// </summary>
+        private static AutoSaveConfig _autoSaveConfig;
+        
+        /// <summary>
+        /// Saving is performed as a task (i.e., while other things may be happening) - as such, we need to provide it
+        /// with an acceptable source for cancelling the task in the form of a "Cancel Token". It's super unlikely that
+        /// we'd cancel saving, but every task needs to be "cancel-able" so we have to play nice and provide it with the
+        /// details it needs.
+        /// Further reading: https://learn.microsoft.com/en-us/dotnet/api/system.threading.cancellationtokensource 
+        /// </summary>
+        private static CancellationTokenSource _autoSaveCancellationTokenSource;
+        
+        /// <summary>
+        /// As mentioned, saving our Unity data is a task that doesn't happen instantly - it takes a small amount of
+        /// time, so this task represents the saving process while it's happening.
+        /// </summary>
+        private static Task _autoSaveTask;
 
+        /// <summary>
+        /// When our project loads - we execute this initialization method.
+        /// </summary>
+        /// <returns></returns>
         [InitializeOnLoadMethod]
-        private static void OnInitialize() {
-            FetchConfig();
+        private static void OnInitialize()
+        {
+            // Grab the AutoSaveConfig (how often to save), but cancel it right now because we don't want to save
+            // immediately - we'll only save every `frequencyMins` interval.
+            FetchAutoSaveConfig();
             CancelTask();
 
-            _tokenSource = new CancellationTokenSource();
-            _task = SaveInterval(_tokenSource.Token);
+            // Create a task cancellation source (tasks require this) and then start the auto-save task!
+            _autoSaveCancellationTokenSource = new CancellationTokenSource();
+            _autoSaveTask = SaveInterval(_autoSaveCancellationTokenSource.Token);
         }
 
-        private static void FetchConfig() {
-            while (true) {
-                if (_config != null) return;
+        /// <summary>
+        /// Method to fetch the current AutoSaveConfig, i.e., the ScriptableObject with details of how often we should
+        /// save!
+        /// </summary>
+        private static void FetchAutoSaveConfig()
+        {
+            // TODO: This is a bit dodgy.. but it keeps trying to find a AutoSaveConfig object, and part of that
+            // "finding" attempt via the `GetConfigPath` method will create one. However, we don't know how long that
+            // will take as the file must be created so we sit in a loop until the file is created.
+            
+            while (true)
+            {
+                // If we already have an AutoSaveConfig then we can exit this method
+                if (_autoSaveConfig != null)
+                {
+                    Debug.LogWarning("AutoSaveConfig exists - auto save frequency is currently: " + _autoSaveConfig.FrequencyMins);
+                    return;
+                }
 
+                // If we didn't already have one, then attempt to find the path to an AutoSaveConfig
                 var path = GetConfigPath();
 
+                // No joy? Create a new AutoSaveConfig using the default values to auto-save once per minute
                 if (path == null) {
                     AssetDatabase.CreateAsset(CreateInstance<AutoSaveConfig>(), $"Assets/{nameof(AutoSaveConfig)}.asset");
-                    Debug.Log("A config file has been created at the root of your project.<b> You can move this anywhere you'd like.</b>");
+                    AssetDatabase.Refresh();
+                    Debug.Log("A AutoSaveConfig file has been created at the root of your project. <b>You can move this anywhere you'd like.</b>");
                     continue;
                 }
 
-                _config = AssetDatabase.LoadAssetAtPath<AutoSaveConfig>(path);
-
+                // Assumed else - we found a path to an AutoSaveConfig, so load it and exit this while loop
+                _autoSaveConfig = AssetDatabase.LoadAssetAtPath<AutoSaveConfig>(path);
                 break;
             }
         }
-
+        
+        /// <summary>
+        /// Method to return the AutoSaveConfig path as string. 
+        /// </summary>
+        /// <returns>The AutoSaveConfig path as a string, or null if no AutoSaveConfig.asset file could be found.</returns>
         private static string GetConfigPath() {
             var paths = AssetDatabase.FindAssets(nameof(AutoSaveConfig)).Select(AssetDatabase.GUIDToAssetPath).Where(c => c.EndsWith(".asset")).ToList();
-            if (paths.Count > 1) Debug.LogWarning("Multiple auto save config assets found. Delete one.");
-            return paths.FirstOrDefault();
+            if (paths.Count > 1) { Debug.LogWarning("Multiple auto save config assets found. Delete one."); }
+            return paths.FirstOrDefault(); // Note: `FirstOrDefault` will return null if no config was found.
         }
 
-        private static void CancelTask() {
-            if (_task == null) return;
-            _tokenSource.Cancel();
-            _task.Wait();
+        /// <summary>
+        /// Method to cancel the auto-save task. 
+        /// </summary>
+        private static void CancelTask()
+        {
+            if (_autoSaveTask == null) return;
+            _autoSaveCancellationTokenSource.Cancel();
+            _autoSaveTask.Wait();
         }
+        
+        /// <summary>
+        /// Method to save all your Unity data.
+        /// </summary>
+        /// <param name="token">The cancellation token required to perform the task (rarely used!)</param>
+        private static async Task SaveInterval(CancellationToken token)
+        {
+            // The save frequency in milliseconds (the original config value is stored in minutes)
+            int saveFrequencyMS = _autoSaveConfig.FrequencyMins * 1000 * 60; 
+            
+            // While the task hasn't been cancelled perform the save operation
+            while (!token.IsCancellationRequested)
+            {
+                Debug.Log("Waiting to save: " + DateTime.Now);
+                
+                await Task.Delay(saveFrequencyMS, token);
+                if (_autoSaveConfig == null) { FetchAutoSaveConfig(); }
 
-        private static async Task SaveInterval(CancellationToken token) {
-            while (!token.IsCancellationRequested) {
-                await Task.Delay(_config.Frequency * 1000 * 60, token);
-                if (_config == null) FetchConfig();
-
-                if (!_config.Enabled || Application.isPlaying || BuildPipeline.isBuildingPlayer || EditorApplication.isCompiling) continue;
+                // Don't save if AutoSave is disabled, or the application is playing or we're building or compiling
+                if (!_autoSaveConfig.Enabled || Application.isPlaying || BuildPipeline.isBuildingPlayer || EditorApplication.isCompiling) continue;
+                
+                // Also don't save when the Unity editor is just sitting in the background inactive!
                 if (!UnityEditorInternal.InternalEditorUtility.isApplicationActive) continue;
 
+                // Otherwise perform the save operation (phew!)
                 EditorSceneManager.SaveOpenScenes();
-                if (_config.Logging) Debug.Log($"Auto-saved at {DateTime.Now:h:mm:ss tt}");
+                if (_autoSaveConfig.LogMsgOnAutoSave) { Debug.Log($"Auto-saved at {DateTime.Now:h:mm:ss tt}"); }
             }
         }
         
-        [MenuItem("Window/Auto save/Find config")]
-        public static void ShowConfig() {
-            FetchConfig();
-
+        /// <summary>
+        /// Method to add a menu item to the `Window` main Unity menu that shows your current AutoSaveConfig.
+        /// </summary>
+        [MenuItem("Window/AutoSave/FindAutoSaveConfig")]
+        public static void ShowConfig()
+        {
+            FetchAutoSaveConfig();
             var path = GetConfigPath();
             EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<AutoSaveConfig>(path).GetInstanceID());
         }
 
+        /// <summary>
+        /// Method to provide details about
+        /// </summary>
+        /// <returns></returns>
         public override void OnInspectorGUI() {
             DrawDefaultInspector();
             EditorGUILayout.Space();
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Unity Auto Save
-Add a simple auto save feature to Unity which fires every set interval. 
+Add a simple auto save feature to Unity which saves all files every set interval (typically once per minute). 
 
 ### User guide<br>
 Put <b>TarodevAutoSave.cs</b> & <b>AutoSaveConfig.cs.cs</b> inside an <b>Editor</b> folder anywhere in your project and it'll generate a configuration file in Assets. You can place this config file anywhere, it won't break.
 
 ### Disclaimer
 Unity could easily implement auto-save functionality, but they haven't for whatever reason. Treat that as a word of warning... It's worked perfectly fine for me without any problems, but I can't guarantee anything.
+
+Last successful test: Unity 2022.2.17 / May 2023.
 
 ### Leave a ⭐ if you found it helpful! Consider supporting me on [Patreon](https://www.patreon.com/tarodev)
 
